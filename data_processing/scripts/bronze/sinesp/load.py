8c1d5c0b622f7a71012fab2b876873e5765312fa
“JnksDavu” <daviandre.junkes@gmail.com>
2025-10-13 23:54:36 -0300

    feat: added load sinesp

---
+++ data_processing/scripts/bronze/sinesp/load.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+import glob
+
+load_dotenv()
+
+USER = os.getenv("USER_DB")
+PASSWORD = os.getenv("PASSWORD_DB")
+HOST = os.getenv("HOST_DB")
+PORT = os.getenv("PORT_DB")
+DB = os.getenv("DB")
+
+engine = create_engine(f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}", future=True)
+
+DATASET_DIR = "/home/tcc/seguranca-publica-brasil/data_processing/dataset/sinesp"
+xlsx_files = sorted(glob.glob(os.path.join(DATASET_DIR, "*.xlsx")))
+
+if not xlsx_files:
+    print(f"Nenhum arquivo XLSX encontrado em {DATASET_DIR}")
+    exit(1)
+
+print(f"{len(xlsx_files)} arquivos encontrados em {DATASET_DIR}:")
+
+for f in xlsx_files:
+    print(f"\nIniciando importação de: {os.path.basename(f)}")
+    df = pd.read_excel(f, sheet_name="in", dtype=str)
+
+    df["data_referencia"] = pd.to_datetime(df["data_referencia"], errors="coerce").dt.date
+    df["feminino"] = pd.to_numeric(df["feminino"], errors="coerce")
+    df["masculino"] = pd.to_numeric(df["masculino"], errors="coerce")
+    df["nao_informado"] = pd.to_numeric(df["nao_informado"], errors="coerce")
+    df["total_vitima"] = pd.to_numeric(df["total_vitima"], errors="coerce")
+    df["total"] = pd.to_numeric(df["total"], errors="coerce")
+    df["total_peso"] = pd.to_numeric(df["total_peso"], errors="coerce")
+
+    for col in df.select_dtypes(include=["object"]).columns:
+        df[col] = df[col].fillna("").astype(str).str.strip()
+
+    print(f"Total de linhas carregadas: {len(df)}")
+
+    with engine.connect() as conn:
+        df.to_sql(
+            "sinesp",
+            con=conn,
+            schema="bronze",
+            if_exists="append",
+            index=False,
+            method="multi",
+            chunksize=10_000
+        )
+
+    print(f"Arquivo {os.path.basename(xlsx_files)} importado com sucesso.")
+
+print("Importação concluída!")
---
+++ data_processing/scripts/bronze/sinesp/run_pipeline.bash
@@ -0,0 +1,38 @@
+set -e
+
+PROJECT_DIR="$(dirname "$(realpath "$0")")"
+
+VENV_DIR="$(realpath "$PROJECT_DIR/../../../venv")"
+
+PYTHON_SCRIPT_PATH="$PROJECT_DIR/load.py"
+REQUIREMENTS_PATH="$(realpath "$PROJECT_DIR/../../../requirements.txt")"
+
+echo "Iniciando pipeline de dados..."
+echo "Diretório do projeto: $PROJECT_DIR"
+echo "Diretório do ambiente virtual: $VENV_DIR"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Ambiente virtual '$VENV_DIR' não encontrado. Criando..."
+    python3 -m venv "$VENV_DIR"
+
+    source "$VENV_DIR/bin/activate"
+    python -m pip install --upgrade pip wheel setuptools
+    pip install -r "$REQUIREMENTS_PATH"
+    deactivate
+    echo "Ambiente virtual criado e dependências instaladas."
+else
+    echo "Ambiente virtual já existente. Verificando dependências..."
+    source "$VENV_DIR/bin/activate"
+    pip install -r "$REQUIREMENTS_PATH" --upgrade --quiet
+    deactivate
+fi
+
+echo "Ativando ambiente virtual para execução..."
+source "$VENV_DIR/bin/activate"
+
+echo "Executando script Python: $PYTHON_SCRIPT_PATH"
+python "$PYTHON_SCRIPT_PATH" "$@"
+
+deactivate
+
+echo "Execução do pipeline finalizada com sucesso!"
